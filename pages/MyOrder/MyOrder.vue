8505cfceafaf3d10c4a81e55428c9c6ed06e1713
Lxc <2081864655@qq.com>
2023-02-18 18:53:26 +0800

    暂无订单

---
+++ pages/MyOrder/MyOrder.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+	<image  class="OrderImage" src="../../static/icon/add_1.png"></image>
+	<view class="Nothing">暂无订单~</view>
+	
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.OrderImage
+{
+	height: 150rpx;
+	width: 150rpx;
+	margin-top: 400rpx;
+	margin-left: 300rpx;
+}
+.Nothing
+{
+	margin-left: 42%;
+	color: gray;
+}
+</style>
