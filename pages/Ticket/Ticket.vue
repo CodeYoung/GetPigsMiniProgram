0fbceae54316ce5d9e3ca2f61bd604ca950ffdfc
Lxc <2081864655@qq.com>
2023-02-19 20:46:44 +0800

    l

---
+++ pages/Ticket/Ticket.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+	<image  class="OrderImage" src="../../static/icon/add_1.png"></image>
+	<view class="Nothing">暂无订单~</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.OrderImage
+	{
+		height: 180rpx;
+		width: 180rpx;
+		margin-top:330rpx;
+		margin-left: 300rpx;
+	}
+	.Nothing
+	{
+		margin-left: 42%;
+		color: #D3D3D3;
+	}
+
+</style>
