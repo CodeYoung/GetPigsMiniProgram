d051ae687ab084afedb92c75a44cb55ca2ad5918
zengy <2170747862@qq.com>
2023-02-27 00:15:58 +0800

    订单交易完成，发布成功，申请成功，订单未完成

---
+++ pages/successfulapplication/successfulapplication.vue
@@ -0,0 +1,51 @@
+<template>
+<view class="box1">
+	<view>.</view>
+	<view  class="box2">
+	<view>
+		<image src="../../static/icon/person.png" class="ImgPicture"></image>
+	</view>
+	<view class="WdApply">申请成功</view>
+	</view>
+	
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.box1{
+		background:#C0C0C0 ;
+		height: 623px;
+		
+	}
+	.box2{
+		border-radius: 10px;
+		margin: 200px;
+		margin-top: 200px;
+		margin-left: 100px;
+		margin-right: 100px;
+		height: 120px;
+		background: #FFFFFF;
+	}
+	.ImgPicture{
+		height: 80rpx;
+		width: 70rpx;
+		
+		margin-top: 24px;
+		
+	margin-left: 72px;
+	margin-right: 100px;
+	
+		
+	}
+	
+    .WdApply{
+		margin-top: 8px;
+			margin-left: 59px;
+			margin-right: 10px;
+		}
+		
+	
+</style>
